afed0d6148e81bb9007babb2156ea874eecce3f0
hjpoe <35583004+hjpoe@users.noreply.github.com>
2019-03-06 15:49:08 -0500

    Create GuessTheNumber.py
    This program allows the user to guess a number between 1 and 10. It uses random, while loops, if statements and a function.

---
+++ GuessTheNumber.py
@@ -0,0 +1,50 @@
+# Author: Harriet Poe
+# This program will allow the user to guess a number
+# between 1 and 10. 
+
+import random
+
+rand = random.randint(1, 10)
+
+# The following function tests a string to see if it
+# is an integer.
+
+
+def represents_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+print("I'm thinking of a number between 1 and 10.")
+
+# Ask the user for a guess and validate it.
+guess = input("What do you think it is?")
+while represents_int(guess) is not True:
+    guess = input("That is not a valid number.")
+
+# If the user is correct on their first guess, let
+# them know they got it right.
+if int(guess) == rand:
+    print("Yay! You got it!")
+
+# Ask the user to guess again until they get the correct answer.
+while int(guess) != rand:
+    # If the user's guess is too low:
+    if int(guess) < rand:
+        guess = int(input("Too low! Guess again:"))
+        # Validate the new guess.
+        while represents_int(guess) is not True:
+            guess = input("That is not a valid number.")
+    else:
+        # If the user's guess is to high:
+        if int(guess) > rand:
+            guess = int(input("Too high! Guess again:"))
+            # Validate the new guess.
+            while represents_int(guess) is not True:
+                guess = input("That is not a valid number.")
+    # Tell the user when they get the answer right.
+    if int(guess) == rand:
+        print("Yay! You got it!")

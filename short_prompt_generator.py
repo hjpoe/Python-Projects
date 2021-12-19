93c4b802f0065312f95ed282b961476cf0bcdd81
H J Poe <35583004+hjpoe@users.noreply.github.com>
2021-12-19 00:58:35 -0500

    Added simple GUI and some new options

--- short_prompt_generator.py
+++ short_prompt_generator.py
@@ -2,15 +2,18 @@
 # This program will generate short story prompt topics
 
 import random
+from tkinter import *
+from tkinter.font import Font
 
 subjectItems = ["woman", "cat", "dog", "man", "fish", "child", "shoe", "sweater", "mouse", "bottle", "lamp", "Teacher",
                 "Student", "Athlete", "Swimmer", "Programmer", "Runner", "Umbrella", "Pirate", "Chicken", "Biker",
                 "Prince", "Princess", "King", "Queen", "girl", "boy"]
 
-connectionItems = ["\b's", "and the", "in the", "on the", "with the", "at the", "of", "\b"]
+connectionItems = ["who had a", "and the", "in the", "on the", "with the", "at the", "of", "who owned a", "of the"]
 
-placeItems = ["ship", "plane", "house", "dewdrop", "car", "truck", "country", "city", "town", "bottle", "school",
-              "office", "pool", "spaceship", "bowl", "plate", "box", "road", "highway", "Palace", "Kingdom"]
+placeItems = ["ship", "plane", "house", "dewdrop", "car", "truck", "country", "city", "town", "village", "bottle",
+              "school", "office", "pool", "spaceship", "bowl", "plate", "cup", "fork", "knife", "spoon", "book", "box",
+              "road", "highway", "path", "Palace", "Kingdom", "Castle"]
 
 randSub = random.randint(0, len(subjectItems) - 1)
 subject = subjectItems[randSub]
@@ -21,4 +24,20 @@ connection = connectionItems[randConn]
 randPlace = random.randint(0, len(placeItems) - 1)
 place = placeItems[randPlace]
 
-print("The", subject, connection, place)
+text = "The" + ' ' + subject + ' ' + connection + ' ' + place
+
+bg_color = '#2a5aad'
+fg_color = '#0b1629'
+
+window = Tk()
+
+window.geometry('480x100')
+window.title("Short Prompt Generator")
+window.config(bg=bg_color)
+font = Font(family='Arial', size=16)
+label = Label(window, text=text, relief='flat')
+label.config(bg=bg_color, fg=fg_color)
+label.config(font=font)
+label.pack(padx=30, pady=30)
+
+window.mainloop()
